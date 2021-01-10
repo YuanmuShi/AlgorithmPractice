ff8309546325a4e9382a91d478e567aed6616081
Jeffrey <guojie.shi01@liulishuo.com>
2021-01-10 15:53:11 +0800

    feat: Resolve 206 reverse list

---
+++ AlgorithmPractice/LeetCode-Go/206-ReverseList.swift
@@ -0,0 +1,34 @@
+//
+//  206-ReverseList.swift
+//  AlgorithmPractice
+//
+//  Created by Jeffrey on 2021/1/10.
+//  Copyright © 2021 Jeffrey. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+  static func test206() {
+    let head = List.createList([1, 2, 3, 4, 5])
+    let _ = Self.reverseList(head)
+  }
+  
+  static func reverseList(_ head: ListNode?) -> ListNode? {
+    guard head?.next != nil else {
+      return head
+    }
+    
+    var preNode: ListNode?
+    var currentNode = head
+    
+    while currentNode != nil {
+      let tmpNode = currentNode?.next
+      currentNode?.next = preNode
+      preNode = currentNode
+      currentNode = tmpNode
+    }
+    
+    return preNode
+  }
+}
