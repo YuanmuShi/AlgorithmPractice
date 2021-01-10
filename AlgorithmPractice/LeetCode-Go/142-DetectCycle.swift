aa1a5242c87587e556d030c4875372dbbceded72
Jeffrey <guojie.shi01@liulishuo.com>
2021-01-10 22:19:36 +0800

    feat: Resolve 142 环形表 II

---
+++ AlgorithmPractice/LeetCode-Go/142-DetectCycle.swift
@@ -0,0 +1,48 @@
+//
+//  142-DetectCycle.swift
+//  AlgorithmPractice
+//
+//  Created by Jeffrey on 2021/1/10.
+//  Copyright © 2021 Jeffrey. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+  static func test142() {
+    let head = List.createList([1, 2, 3, 4, 5, 6, 7], cycleNodeValue: 3)
+    List.showListContent(with: head, preText: "Original List")
+    let result = Self.detectCycle(head)
+    print("Cycle Node: \(result?.val)")
+  }
+  
+  private static func detectCycle(_ head: ListNode?) -> ListNode? {
+    guard head != nil else {
+      return nil
+    }
+    
+    var slowNode = head
+    var fastNode = head
+    
+    repeat {
+      fastNode = fastNode?.next?.next
+      slowNode = slowNode?.next
+      if fastNode === slowNode {
+        break
+      }
+    } while fastNode != nil
+    
+    if fastNode == nil {
+      return nil
+    }
+    
+    fastNode = head
+    
+    while fastNode !== slowNode {
+      fastNode = fastNode?.next
+      slowNode = slowNode?.next
+    }
+    
+    return fastNode
+  }
+}
---
+++ AlgorithmPractice/LeetCode-Go/25-ReverseKGroup.swift
@@ -0,0 +1,41 @@
+//
+//  25-ReverseKGroup.swift
+//  AlgorithmPractice
+//
+//  Created by Jeffrey on 2021/1/10.
+//  Copyright © 2021 Jeffrey. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+  static func test25() {
+    let head = List.createList([1, 2, 3, 4, 5])
+    List.showListContent(with: head, preText: "Original List")
+    let resNode = Self.reverseKGroup(head, 3)
+    List.showListContent(with: resNode, preText: "Result List")
+  }
+  
+  private static func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
+    guard head?.next != nil, k > 1 else {
+      return head
+    }
+    
+    var preNode: ListNode?
+    var segmentHead = head
+    var segmentTrail = head
+    
+    (0..<k).forEach { _ in
+      segmentTrail = segmentTrail?.next
+    }
+    
+    var nextHead = segmentTrail?.next
+    
+    
+    
+    
+    
+    
+    return preNode
+  }
+}
