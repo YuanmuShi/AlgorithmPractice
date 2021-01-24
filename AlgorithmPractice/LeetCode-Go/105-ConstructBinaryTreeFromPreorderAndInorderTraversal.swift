59d481361a7d4c7dd6cc1bada7bb029a1d50eabd
Jeffrey <guojie.shi01@liulishuo.com>
2021-01-24 23:06:59 +0800

    feat: 105

--- AlgorithmPractice/LeetCode-Go/105-ConstructBinaryTreeFromPreorderAndInorderTraversal.swift
+++ AlgorithmPractice/LeetCode-Go/105-ConstructBinaryTreeFromPreorderAndInorderTraversal.swift
@@ -9,15 +9,15 @@
 import Foundation
 
 /*
- 根据一棵树的中序遍历与后序遍历构造二叉树。
+ 根据一棵树的前序遍历与中序遍历构造二叉树。
 
  注意:
  你可以假设树中没有重复的元素。
 
  例如，给出
 
- 中序遍历 inorder = [9,3,15,20,7]
- 后序遍历 postorder = [9,15,7,20,3]
+ 前序遍历 preorder = [3,9,20,15,7]
+ 中序遍历 inorder = [9,3,15,20,7]
  返回如下的二叉树：
 
      3
@@ -27,14 +27,32 @@ import Foundation
     15   7
 
  来源：力扣（LeetCode）
- 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal
+ 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 extension Solution {
-  static func test105() {}
+  static func test105() {
+    let root = buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7])
+//    let root = buildTree([1, 2, 3], [3, 2, 1])
+    print(root!.asString)
+  }
 
-  private static func buildTree(_ inorder: [Int], _ postorder: [Int]) -> TreeNode? {
-    return nil
+  private static func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {
+    guard !preorder.isEmpty, !inorder.isEmpty else { return nil }
+
+    let rootVal = preorder[0]
+    let root = TreeNode(rootVal)
+    
+    let leftInorder = Array(inorder.prefix(inorder.firstIndex(of: rootVal)!))
+    let leftPreorder = Array(preorder.prefix(leftInorder.count + 1).dropFirst())
+
+    let rightPreorder = Array(preorder.suffix(preorder.count - leftPreorder.count - 1))
+    let rightInorder = Array(inorder.suffix(preorder.count - leftPreorder.count - 1))
+
+    root.left = buildTree(leftPreorder, leftInorder)
+    root.right = buildTree(rightPreorder, rightInorder)
+
+    return root
   }
 }
