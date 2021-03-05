cfc016cad43dea81f75a00e2300d62a0cc7a1ec3
Jeffrey <guojie.shi01@liulishuo.com>
2021-03-05 12:57:02 +0800

    feat: 232 优化方案

--- AlgorithmPractice/LeetCode-Go/232-MyQueue.swift
+++ AlgorithmPractice/LeetCode-Go/232-MyQueue.swift
@@ -18,18 +18,15 @@ import Foundation
  int pop() 从队列的开头移除并返回元素
  int peek() 返回队列开头的元素
  boolean empty() 如果队列为空，返回 true ；否则，返回 false
-  
 
  说明：
 
  你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
  你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
-  
 
  进阶：
 
  你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
-  
 
  示例：
 
@@ -46,7 +43,6 @@ import Foundation
  myQueue.peek(); // return 1
  myQueue.pop(); // return 1, queue is [2]
  myQueue.empty(); // return false
-  
 
  提示：
 
@@ -57,7 +53,6 @@ import Foundation
 
 extension Solution {
   static func test232() {
-    
     let q = MyQueue()
     q.push(1)
     q.push(2)
@@ -70,43 +65,76 @@ extension Solution {
   }
 }
 
-
+// 优化方案：因为 removeStack 存放的是队列的前 n 个点，addStack 存放的是队列的后 m 个点，因此，每次 push 时无需再将 removeStack 元素都放回 addStack，使得 push 的时间复杂度从 O(n) 降到了 O(1)
 class MyQueue {
-  
   var addStack = MyStack()
   var removeStack = MyStack()
-  
-  /** Initialize your data structure here. */
-  init() {
-    
-  }
-  
-  /** Push element x to the back of queue. */
+
+  init() {}
+
   func push(_ x: Int) {
-    while removeStack.top() > 0 {
-      addStack.push(removeStack.pop())
-    }
     addStack.push(x)
   }
-  
-  /** Removes the element from in front of queue and returns that element. */
+
   func pop() -> Int {
-    while addStack.top() > 0 {
-      removeStack.push(addStack.pop())
+    if removeStack.empty() {
+      while addStack.top() > 0 {
+        removeStack.push(addStack.pop())
+      }
     }
     return removeStack.pop()
   }
-  
-  /** Get the front element. */
+
   func peek() -> Int {
-    while addStack.top() > 0 {
-      removeStack.push(addStack.pop())
+    if removeStack.empty() {
+      while addStack.top() > 0 {
+        removeStack.push(addStack.pop())
+      }
     }
     return removeStack.top()
   }
-  
-  /** Returns whether the queue is empty. */
+
   func empty() -> Bool {
     return addStack.empty() && removeStack.empty()
   }
 }
+
+/*
+ // 常规解法，push pop 以及 peek 都是O(n)时间复杂度
+ class MyQueue {
+   var addStack = MyStack()
+   var removeStack = MyStack()
+
+   /** Initialize your data structure here. */
+   init() {}
+
+   /** Push element x to the back of queue. */
+   func push(_ x: Int) {
+     while removeStack.top() > 0 {
+       addStack.push(removeStack.pop())
+     }
+     addStack.push(x)
+   }
+
+   /** Removes the element from in front of queue and returns that element. */
+   func pop() -> Int {
+     while addStack.top() > 0 {
+       removeStack.push(addStack.pop())
+     }
+     return removeStack.pop()
+   }
+
+   /** Get the front element. */
+   func peek() -> Int {
+     while addStack.top() > 0 {
+       removeStack.push(addStack.pop())
+     }
+     return removeStack.top()
+   }
+
+   /** Returns whether the queue is empty. */
+   func empty() -> Bool {
+     return addStack.empty() && removeStack.empty()
+   }
+ }
+ */
